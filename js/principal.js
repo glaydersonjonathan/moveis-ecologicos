21b8cad8a5604dae5c794ff6d2d9a567252ec384
Glayderson Jonathan Nunes <glaydersonjonathan@gmail.com>
2016-09-22 12:11:58 -0300

    Conserto do javascript

--- js/principal.js
+++ js/principal.js
@@ -3,8 +3,9 @@ var bannerAtual = 0;
 
 function trocaBanner() {
 	bannerAtual = (bannerAtual + 1) % 2;
-	document.querySelector(’h2#mensagem’).textContent =
+	document.querySelector("h2#mensagem").textContent =
 		banners[bannerAtual];
 }
 
 setInterval(trocaBanner, 1000);
+
